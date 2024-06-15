6076adc3ca00670a587ce5b70ea300c783dde24d
Ursula Jardon <155091638+gar9312@users.noreply.github.com>
2024-06-15 21:43:17 +0000

    finish

--- src/js/app.js
+++ src/js/app.js
@@ -40,23 +40,23 @@ function render(variables = {}) {
           <h3>${variables.city ? variables.city : "CDMX"} , ${
     variables.country ? variables.country : "Mexico"
   } </h3>
-          <ul class="${variables.socialMediaPosition}">
-            <li><a href= ${
+          <ul class={variables.socialMediaPosition}>
+            <li><a href={
               variables.twitter
                 ? "https://twitter.com/" + variables.twitter
                 : "https://twitter.com/4geeksacademy"
             } ><i class="fab fa-twitter"></i></a> </li>
-            <li><a href="${
+            <li><a href={
               variables.github
                 ? "https://github.com/" + variables.github
                 : "https://github.com/4geeksacademy"
             } ><i class="fab fa-github"></i></a> </li>
-            <li><a href="${
+            <li><a href={
               variables.linkedin
                 ? "https://linkedin.com/school/" + variables.linkedin
                 : "https://linkedin.com/school/4geeksacademy"
             }><i class="fab fa-linkedin"></i></a> </li>
-            <li><a href="${
+            <li><a href={
               variables.instagram
                 ? "https://instagram.com/" + variables.instagram
                 : "https://instagram.com/4geeksacademy"
